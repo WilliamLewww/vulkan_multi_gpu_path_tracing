0f384ac65a6a0799b38bb1a0758cf4fdfdcbf6aa
WilliamLewww <WilliamMilesLew@gmail.com>
2020-11-18 21:02:36 -0800

    cleaned: vulkan structure formatting in device swapchain class

--- src/device_swapchain.cpp
+++ src/device_swapchain.cpp
@@ -23,15 +23,26 @@ DeviceSwapchain::DeviceSwapchain(VkDevice logicalDevice, VkPhysicalDevice physic
     this->swapchainImageCount = surfaceCapabilities.maxImageCount;
   }
 
-  VkSwapchainCreateInfoKHR swapchainCreateInfo = {};
-  swapchainCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
-  swapchainCreateInfo.surface = surface;
-  swapchainCreateInfo.minImageCount = this->swapchainImageCount;
-  swapchainCreateInfo.imageFormat = surfaceFormat.format;
-  swapchainCreateInfo.imageColorSpace = surfaceFormat.colorSpace;
-  swapchainCreateInfo.imageExtent = extent;
-  swapchainCreateInfo.imageArrayLayers = 1;
-  swapchainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT;
+  VkSwapchainCreateInfoKHR swapchainCreateInfo = {
+    .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+    .pNext = NULL,
+    .flags = 0,
+    .surface = surface,
+    .minImageCount = this->swapchainImageCount,
+    .imageFormat = surfaceFormat.format,
+    .imageColorSpace = surfaceFormat.colorSpace,
+    .imageExtent = extent,
+    .imageArrayLayers = 1,
+    .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
+    .imageSharingMode = VK_SHARING_MODE_EXCLUSIVE,
+    .queueFamilyIndexCount = 0,
+    .pQueueFamilyIndices = NULL,
+    .preTransform = surfaceCapabilities.currentTransform,
+    .compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+    .presentMode = presentMode,
+    .clipped = VK_TRUE,
+    .oldSwapchain = VK_NULL_HANDLE,
+  };
 
   if (graphicsQueueIndex != presentQueueIndex) {
     uint32_t queueFamilyIndices[2] = {graphicsQueueIndex, presentQueueIndex};
@@ -43,12 +54,6 @@ DeviceSwapchain::DeviceSwapchain(VkDevice logicalDevice, VkPhysicalDevice physic
   else {
     swapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
   }
-
-  swapchainCreateInfo.preTransform = surfaceCapabilities.currentTransform;
-  swapchainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
-  swapchainCreateInfo.presentMode = presentMode;
-  swapchainCreateInfo.clipped = VK_TRUE;
-  swapchainCreateInfo.oldSwapchain = VK_NULL_HANDLE;
 
   if (vkCreateSwapchainKHR(logicalDevice, &swapchainCreateInfo, NULL, &this->swapchain) != VK_SUCCESS) {
     printf("failed to create swapchain\n");
@@ -64,20 +69,27 @@ DeviceSwapchain::DeviceSwapchain(VkDevice logicalDevice, VkPhysicalDevice physic
   this->swapchainImageViewList.resize(this->swapchainImageCount);
 
   for (int x = 0; x < this->swapchainImageCount; x++) {
-    VkImageViewCreateInfo imageViewCreateInfo = {};
-    imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-    imageViewCreateInfo.image = this->swapchainImageList[x];
-    imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-    imageViewCreateInfo.format = this->swapchainImageFormat;
-    imageViewCreateInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
-    imageViewCreateInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
-    imageViewCreateInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
-    imageViewCreateInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
-    imageViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-    imageViewCreateInfo.subresourceRange.baseMipLevel = 0;
-    imageViewCreateInfo.subresourceRange.levelCount = 1;
-    imageViewCreateInfo.subresourceRange.baseArrayLayer = 0;
-    imageViewCreateInfo.subresourceRange.layerCount = 1;
+    VkImageViewCreateInfo imageViewCreateInfo = {
+      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+      .pNext = NULL,
+      .flags = 0,
+      .image = this->swapchainImageList[x],
+      .viewType = VK_IMAGE_VIEW_TYPE_2D,
+      .format = this->swapchainImageFormat,
+      .components = {
+        .r = VK_COMPONENT_SWIZZLE_IDENTITY,
+        .g = VK_COMPONENT_SWIZZLE_IDENTITY,
+        .b = VK_COMPONENT_SWIZZLE_IDENTITY,
+        .a = VK_COMPONENT_SWIZZLE_IDENTITY,
+      },
+      .subresourceRange = {
+        .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+        .baseMipLevel = 0,
+        .levelCount = 1,
+        .baseArrayLayer = 0,
+        .layerCount = 1
+      }
+    };
 
     if (vkCreateImageView(logicalDevice, &imageViewCreateInfo, NULL, &this->swapchainImageViewList[x]) != VK_SUCCESS) {
       printf("failed to create image view #%d\n", x);
