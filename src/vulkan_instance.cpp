c2427f6a3d26983663f120422a7acfb92a7b429e
WilliamLewww <WilliamMilesLew@gmail.com>
2020-11-18 20:49:01 -0800

    cleaned: vulkan structure formatting for vulkan instance class

--- src/vulkan_instance.cpp
+++ src/vulkan_instance.cpp
@@ -17,31 +17,39 @@ VulkanInstance::VulkanInstance(const char* applicationName, std::vector<const ch
     this->extensionList.push_back(extensionList[x]);
   }
 
-  VkApplicationInfo applicationInfo = {};
-  applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-  applicationInfo.pApplicationName = applicationName;
-  applicationInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-  applicationInfo.pEngineName = "No Engine";
-  applicationInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-  applicationInfo.apiVersion = VK_API_VERSION_1_2;
+  VkApplicationInfo applicationInfo = {
+    .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+    .pNext = NULL,
+    .pApplicationName = applicationName,
+    .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+    .pEngineName = "No Engine",
+    .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+    .apiVersion = VK_API_VERSION_1_2
+  };
   
-  VkInstanceCreateInfo instanceCreateInfo = {};
-  instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-  instanceCreateInfo.flags = 0;
-  instanceCreateInfo.pApplicationInfo = &applicationInfo;
-  instanceCreateInfo.enabledExtensionCount = this->extensionList.size();
-  instanceCreateInfo.ppEnabledExtensionNames = &this->extensionList[0];
-  instanceCreateInfo.enabledLayerCount = layerList.size();
+  VkInstanceCreateInfo instanceCreateInfo = {
+    .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+    .pNext = NULL,
+    .flags = 0,
+    .pApplicationInfo = &applicationInfo,
+    .enabledLayerCount = (uint32_t)layerList.size(),
+    .ppEnabledLayerNames = layerList.data(),
+    .enabledExtensionCount = (uint32_t)this->extensionList.size(),
+    .ppEnabledExtensionNames = &this->extensionList[0],
+  };
 
   if (enableValidation) {
-    VkDebugUtilsMessengerCreateInfoEXT messengerCreateInfo = {};
-    messengerCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-    messengerCreateInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-    messengerCreateInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-    messengerCreateInfo.pfnUserCallback = debugCallback;
+    VkDebugUtilsMessengerCreateInfoEXT messengerCreateInfo = {
+      .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+      .pNext = NULL,
+      .flags = 0,
+      .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+      .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+      .pfnUserCallback = debugCallback,
+      .pUserData = NULL
+    };
 
     instanceCreateInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&messengerCreateInfo;
-    instanceCreateInfo.ppEnabledLayerNames = &layerList[0];
 
     if (vkCreateInstance(&instanceCreateInfo, NULL, &this->instance) != VK_SUCCESS) {
       printf("failed to create Vulkan instance\n");
