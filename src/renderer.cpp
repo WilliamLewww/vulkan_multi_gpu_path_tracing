1bf1b51876282192d6706c85a061703582f74606
WilliamLewww <WilliamMilesLew@gmail.com>
2020-12-03 17:26:58 -0800

    added: scene with transparent border around transparent object

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -27,7 +27,7 @@ Renderer::Renderer(VkInstance vulkanInstance, VkSurfaceKHR surface, ModelCollect
 
   std::map<Model*, std::vector<Matrix4x4>> modelFrequencyMap = {
     {
-      modelCollection->getModel(3), 
+      modelCollection->getModel(5), 
       {
         createTranslateMatrix4x4(0, 0, 0)
       }
